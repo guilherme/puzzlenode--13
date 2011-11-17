0792a0b591e278bfbe27256313c27f824679d231
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2011-11-17 14:27:46 -0200

    adding complex and simple board files

---
+++ simple_board.rb
@@ -0,0 +1,27 @@
+require 'chess_validator'
+board = Chess::Board.parse!("
+bR bN bB bQ bK bB bN bR
+bP bP bP bP bP bP bP bP
+-- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- --
+wP wP wP wP wP wP wP wP
+wR wN wB wQ wK wB wN wR")
+puts board.to_s
+
+"a2 a3
+a2 a4
+a2 a5
+a7 a6
+a7 a5
+a7 a4
+a7 b6
+b8 a6
+b8 c6
+b8 d7
+e2 e3
+e3 e2".each_line do |l|
+ move = board.move!(l)
+ board.reset!
+end
