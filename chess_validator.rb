8bb4febffe679891841266d6c9f66047b2f26a20
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2011-11-17 14:28:01 -0200

    seems to be ok

--- chess_validator.rb
+++ chess_validator.rb
@@ -463,22 +463,27 @@ module Chess
 
   class King < Piece
     def possible_moves(current_position, board)
-      #TODO: CHECK IF ANOTHER PIECE 
-      #TODO: CHECK IF TRESPASS THE PIECE
       moves = []
       # PARA CIMA E PARA BAIXO
-      moves.push(Cordinate.new(current_position.row+1,current_position.column))
-      moves.push(Cordinate.new(current_position.row-1,current_position.column))
+      position = current_position.clone; position.step_up!
+      moves.push(position)
+      position = current_position.clone; position.step_down!
+      moves.push(position)
       # PARA UM LADO E PARA O OUTRO
-      moves.push(Cordinate.new(current_position.row,current_position.column+1))
-      moves.push(Cordinate.new(current_position.row,current_position.column-1))
+      position = current_position.clone; position.step_left!
+      moves.push(position)
+      position = current_position.clone; position.step_right!
+      moves.push(position)
       # NO EIXO DA DIAGONAL DIREITA
-      moves.push(Cordinate.new(current_position.row+1, current_position.column+1))
-      moves.push(Cordinate.new(current_position.row-1, current_position.column-1))
+      position = current_position.clone; position.step_right_up_diagonal!
+      moves.push(position)
+      position = current_position.clone; position.step_right_down_diagonal!
+      moves.push(position)
       # NO EIXO DA DIAGONAL ESQUERDA
-      moves.push(Cordinate.new(current_position.row+1,current_position.column-1))
-      moves.push(Cordinate.new(current_position.row-1,current_position.column+1))
-
+      position = current_position.clone; position.step_left_up_diagonal!
+      moves.push(position)
+      position = current_position.clone; position.step_left_down_diagonal!
+      moves.push(position)
 
       moves
     end
@@ -489,30 +494,29 @@ module Chess
 
   class Knight < Piece
     def possible_moves(current_position, board)
-      #TODO: CHECK IF ANOTHER PIECE 
-      #TODO: CHECK IF TRESPASS THE PIECE
+      # OK
       moves = []
       # UP 
       position = Cordinate.new(current_position.row+2,current_position.column-1)
-      moves.push(position) if board.piece_at(position).nil?  || board.piece_at(position).color != self.color
+      moves.push(position)
 
       position = Cordinate.new(current_position.row+2,current_position.column+1)
-      moves.push(position) if board.piece_at(position).nil?  || board.piece_at(position).color != self.color
+      moves.push(position)
       # DOWN
       position = Cordinate.new(current_position.row-2,current_position.column-1)
-      moves.push(position) if board.piece_at(position).nil?  || board.piece_at(position).color != self.color
+      moves.push(position)
       position = Cordinate.new(current_position.row-2,current_position.column+1)
-      moves.push(position) if board.piece_at(position).nil?  || board.piece_at(position).color != self.color
+      moves.push(position)
       # LEFT                                         ,                         )
       position = Cordinate.new(current_position.row+1,current_position.column-2)
-      moves.push(position) if board.piece_at(position).nil?  || board.piece_at(position).color != self.color
+      moves.push(position)
       position = Cordinate.new(current_position.row-1,current_position.column-2)
-      moves.push(position) if board.piece_at(position).nil?  || board.piece_at(position).color != self.color
+      moves.push(position)
       # RIGHT                                        ,                         )
       position = Cordinate.new(current_position.row+1,current_position.column+2)
-      moves.push(position) if board.piece_at(position).nil?  || board.piece_at(position).color != self.color
+      moves.push(position)
       position = Cordinate.new(current_position.row-1,current_position.column+2)
-      moves.push(position) if board.piece_at(position).nil?  || board.piece_at(position).color != self.color
+      moves.push(position) 
 
       moves
     end
@@ -522,8 +526,6 @@ module Chess
   end
   class Pawn < Piece
     def possible_moves(current_position, board)
-      #TODO: CHECK IF ANOTHER PIECE 
-      #TODO: CHECK IF TRESPASS THE PIECE
       moves = []
       # UP 
       if self.color == "b"
@@ -589,174 +591,4 @@ end
 
 
 
-=begin
-board = Chess::Board.parse!("
-bR bN bB bQ bK bB bN bR
-bP bP bP bP bP bP bP bP
--- -- -- -- -- -- -- --
--- -- -- -- -- -- -- --
--- -- -- -- -- -- -- --
--- -- -- -- -- -- -- --
-wP wP wP wP wP wP wP wP
-wR wN wB wQ wK wB wN wR")
-puts board.to_s == "
-bR bN bB bQ bK bB bN bR
-bP bP bP bP bP bP bP bP
--- -- -- -- -- -- -- --
--- -- -- -- -- -- -- --
--- -- -- -- -- -- -- --
--- -- -- -- -- -- -- --
-wP wP wP wP wP wP wP wP
-wR wN wB wQ wK wB wN wR".strip.chomp
-puts board.to_s
 
-"a2 a3
-a2 a4
-a2 a5
-a7 a6
-a7 a5
-a7 a4
-a7 b6
-b8 a6
-b8 c6
-b8 d7
-e2 e3
-e3 e2".each_line do |l|
- move = board.move!(l)
- board.reset!
-end
-=end
-
-complex_board = Chess::Board.parse!("
-bK -- -- -- -- bB -- --
--- -- -- -- -- bP -- --
--- bP wR -- wB -- bN --
-wN -- bP bR -- -- -- wP
--- -- -- -- wK wQ -- wP
-wR -- bB wN wP -- -- --
--- wP bQ -- -- wP -- --
--- -- -- -- -- wB -- --")
-puts complex_board.to_s
-
-"b2 b3 
-f2 b7  
-b4 a8  
-a8 g5  
-b2 b4
-h7 f6
-e3 b1
-b7 e4
-b2 b5
-g7 g2
-f7 f8
-g7 e8
-f5 g5
-f7 f6
-c3 a2
-e1 c1
-f7 f5
-g8 c6
-b2 d5
-f7 f4
-f6 f3
-c5 f4
-b6 a5
-c3 d7
-b2 f6
-e4 d8
-d2 g6
-b2 c3
-a3 g5
-h6 e8
-d3 b5
-d6 f3
-c5 c4
-f6 a3
-b7 c1
-f1 f1
-b6 b4
-a7 e5
-b6 c5
-c2 e7
-d4 f5
-a7 g8
-a7 d1
-c5 b4
-c4 e3
-h4 h3
-b5 b2
-b2 g4
-h3 h4
-h8 d3
-h8 h7
-h2 h3
-e8 c4
-a3 a1
-d3 b5
-a3 a1
-d5 d3
-d5 f5
-b4 f6
-d5 g2
-c8 d4
-d5 b5
-c6 d5
-f3 b5
-a1 g6
-g6 e5
-d3 e6
-g6 a7
-g6 h4
-c6 d8
-a3 h8
-g8 f8
-c7 f2
-d3 c4
-a5 a4
-a5 b5
-f1 a8
-c2 g1
-e5 a4
-h5 f1
-e6 g4
-g5 h2
-c3 b2
-h2 c7
-e6 f3
-c3 c4
-f1 c4
-b3 a6
-c4 b3
-c1 h6
-a5 c5
-f4 f7
-c2 c6
-f6 g2
-d2 a1
-e4 a3
-c2 d3
-a8 c6
-d6 d1
-c2 e4
-d6 g2
-a6 a7
-c2 c4
-a8 a7
-c4 d3
-c3 h5
-b4 c7
-e6 d5
-b6 d6
-e5 f8
-f1 f6
-a8 a6
-a8 c6
-e4 e5
-h2 a5
-c8 e1
-e3 h2
-f3 h3".each_line do |l|
- move = complex_board.move!(l)
- puts move.to_s
- complex_board.reset!
-end
